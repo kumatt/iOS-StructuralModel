a9c9c3b7c94c2e32bb72323404b5613e890137a5
OComme <a163913692@icloud.com>
2017-09-27 16:58:20 +0800

    file name changed

--- ExampleProjectDemo/PrefixHeader/DefineHeader.h
+++ ExampleProjectDemo/PrefixHeader/DefineHeader.h
@@ -11,6 +11,12 @@
 #ifndef DefineHeader_h
 #define DefineHeader_h
 
+//打印信息
+#ifdef DEBUG
+#define NSLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define NSLog(...)
+#endif
 
 //屏幕宽
 #define CUSTOM_SCREEN_WIDTH  [UIScreen mainScreen].bounds.size.width
@@ -24,6 +30,7 @@
 //像素等比例转换屏幕尺寸
 #define CUSTOM_PX(value) ((value)/750.0f*[UIScreen mainScreen].bounds.size.width)
 
+//------------------color----------------
 //16进制颜色
 #define CUSTOM_COLOR_HEX(rgbValue) \
 [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 \
@@ -36,16 +43,27 @@ blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:1.0]
 // 随机色
 #define CUSTOM_COLOR_RANDOM RGBColor(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
 
-//打印信息
-#ifdef DEBUG
+//------------------typeof----------------
+///弱引用
+#define CUSTOM_TYPEWEAK(type)  __weak typeof(type) weak##type = type;
 
-#define MyLog(...)  NSLog(__VA_ARGS__)
+///强引用
+#define CUSTOM_TYPESTRONG(type)  __strong typeof(type) type = weak##type;
 
-#else
-
-#define MyLog(...)
-
-#endif
-
+////------------------path----------------
+/////拼接Document路径
+//#define CUSTOM_PATHDocument [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject]
+//
+//#define CUSTOM_PATHCache [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject]
+//
+////------------------Thread----------------
+/////GCD代码只执行一次
+//#define CUSTOM_DISPATCH_ONCE(onceBlock) static dispatch_once_t onceToken; dispatch_once(&onceToken, onceBlock);
+//
+/////在主线程上运行
+//#define CUSTOM_DISPATCH_MAIN(mainQueueBlock) dispatch_async(dispatch_get_main_queue(), mainQueueBlock);
+//
+/////开启异步线程
+//#define CUSTOM_DISPATCH_GLOBAL_QUEUE_DEFAULT(globalQueueBlock) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), globalQueueBlocl);
 
 #endif /* DefineHeader_h */
